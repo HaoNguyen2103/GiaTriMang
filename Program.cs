cc255227bd81a9c3032d9aa22f4b3f97374052f9
HaoNguyen2103 <nguyenhao21031995@gmail.com>
2025-08-16 21:36:44 +0700

    add
    Vi tri sinh vien

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] students = new[] { "Chistian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Xachariah", "Mallory", "Zoe", "Emily" };
+        Console.WriteLine("Nhap ten cua sinh vien: ");
+        string input_name = Console.ReadLine();
+        bool isExist = false;
+        for (int i = 0; i < students.Length; i++)
+        {
+            if (students[i].Equals(input_name))
+            {
+                Console.WriteLine("Vi tri cua hoc sinh trong danh sach " + input_name + " la: " + (i + 1));
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist)
+        {
+        Console.WriteLine("Khong tim thay" + input_name + "trong danh sach"); 
+        }
+    }
+}
